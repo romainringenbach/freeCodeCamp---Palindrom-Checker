ff8d1e1c44d3ccb7fc4e5db217e1cbb5799df1b1
romainringenbach <5659380+romainringenbach@users.noreply.github.com>
2024-05-19 18:19:01 +0200

    Add alert on empty string and fix checking

--- script.js
+++ script.js
@@ -4,16 +4,24 @@ const resultDiv = document.getElementById("result");
 
 checkBtn.addEventListener("click",() => {
     const valueToCheck = input.value
+    if(valueToCheck === ""){
+        alert("Please input a value");
+        return;
+    }
     const isPalindrome = checkPalindrome(valueToCheck);
     let msg = `${valueToCheck} is ${isPalindrome? "" : "not "}a palindrome`;
     resultDiv.textContent = msg;
 });
 
 function checkPalindrome(value){
+    if(value.length === 1){
+      return true;
+    }
+
     const regex = /[\W_]/g
     const cleanedValue = value.replace(regex,"").toLowerCase();
     const firstPart = cleanedValue.slice(0, Math.ceil(cleanedValue.length/2));
-    const secondPart = cleanedValue.slice(Math.ceil(cleanedValue.length/2), cleanedValue.length);
+    const secondPart = cleanedValue.slice(Math.floor(cleanedValue.length/2), cleanedValue.length);
     const secondPartReverted = Array.from(secondPart).reverse().join("");
     return firstPart === secondPartReverted;
 }
