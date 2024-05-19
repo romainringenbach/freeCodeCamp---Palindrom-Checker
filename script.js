2d5398f72f83e29207b977d172617cbac3983ef6
romainringenbach <5659380+romainringenbach@users.noreply.github.com>
2024-05-19 17:35:37 +0200

    Add palindrome checking

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Hello World!");
+const checkBtn = document.getElementById("check-btn");
+const input = document.getElementById("text-input");
+const resultDiv = document.getElementById("result");
+
+checkBtn.addEventListener("click",() => {
+    const valueToCheck = input.value
+    const isPalindrome = checkPalindrome(valueToCheck);
+    let msg = `${valueToCheck} is ${isPalindrome? "" : "not "}a palindrome`;
+    resultDiv.textContent = msg;
+});
+
+function checkPalindrome(value){
+    const regex = /[\W_]/g
+    const cleanedValue = value.replace(regex,"").toLowerCase();
+    const firstPart = cleanedValue.slice(0, Math.ceil(cleanedValue.length/2));
+    const secondPart = cleanedValue.slice(Math.ceil(cleanedValue.length/2), cleanedValue.length);
+    const secondPartReverted = Array.from(secondPart).reverse().join("");
+    return firstPart === secondPartReverted;
+}
+
